8059820ab1e87ed006d37e330a73001bdb41528c
Krishan Thisera <krishan.thisera@aligent.com.au>
2023-07-10 20:06:50 +0930

    update terraform outputs

--- outputs.tf
+++ outputs.tf
@@ -6,3 +6,7 @@ output "assets_bucket_domain_name" {
 output "cloudfront_arn" {
   value = aws_cloudfront_distribution.blog_distribution.arn
 }
+
+output "cloudfront_domain_name" {
+    value = aws_cloudfront_distribution.blog_distribution.domain_name
+}
