c66484fbcff7351e7e22bc87babdbfd61f2ced86
Krishan Thisera <krishan.thisera@aligent.com.au>
2023-07-25 13:49:32 +0930

    comment acm configuration

--- acm.tf
+++ acm.tf
@@ -1,21 +1,21 @@
-# SSL Certificate
-resource "aws_acm_certificate" "ssl_certificate" {
-  count = var.provission_acm == true ? 1 : 0
-  provider                  = aws.acm_provider
-  domain_name               = var.domain_name
-  subject_alternative_names = ["*.${var.domain_name}"]
-  validation_method         = var.acm_validatio_method
+# # SSL Certificate
+# resource "aws_acm_certificate" "ssl_certificate" {
+#   count = var.provission_acm == true ? 1 : 0
+#   provider                  = aws.acm_provider
+#   domain_name               = var.domain_name
+#   subject_alternative_names = ["*.${var.domain_name}"]
+#   validation_method         = var.acm_validatio_method
 
-  tags = var.common_tags
+#   tags = var.common_tags
 
-  lifecycle {
-    create_before_destroy = true
-  }
-}
+#   lifecycle {
+#     create_before_destroy = true
+#   }
+# }
 
-# SSL Certificate Validation - DNS
-resource "aws_acm_certificate_validation" "cert_validation" {
-  count = var.acm_validatio_method == "DNS" ? 1 : 0
-  provider        = aws.acm_provider
-  certificate_arn = aws_acm_certificate.ssl_certificate.arn
-}
+# # SSL Certificate Validation - DNS
+# resource "aws_acm_certificate_validation" "cert_validation" {
+#   count = var.acm_validatio_method == "DNS" ? 1 : 0
+#   provider        = aws.acm_provider
+#   certificate_arn = aws_acm_certificate.ssl_certificate.arn
+# }
