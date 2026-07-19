9d15b8ef1066f7fe80b9218272384b45d240e7ca
Krishan Thisera <krishan.thisera@cm3.com.au>
2026-07-19 14:24:36 +1000

    fix: typo in the edge module

--- edge-functions.tf
+++ edge-functions.tf
@@ -1,5 +1,5 @@
-module "edge-functions" {
-  source = "github.com/krishantha/aws-edge-functions"
+module "edge_functions" {
+  source = "github.com/krishanthisera/aws-edge-functions"
 
   providers = {
     aws = aws.us_east_1
