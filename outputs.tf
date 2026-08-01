25a5ad191f389c9de4b309b89cf6ba023506eb8b
krishanthisera <krishan.thisera@yahoo.com>
2026-08-01 12:46:16 +1000

    feat: update the outputs

--- outputs.tf
+++ outputs.tf
@@ -10,3 +10,7 @@ output "cloudfront_arn" {
 output "cloudfront_domain_name" {
   value = aws_cloudfront_distribution.blog_distribution.domain_name
 }
+
+output "cloudfront_distribution_id" {
+  value = aws_cloudfront_distribution.blog_distribution.id
+}
